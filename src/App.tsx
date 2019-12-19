2d263ebaf1e381ce425827a74bfd85a880eea482
Ahmad Hamid <ahamidEng@outlook.com>
2019-12-19 08:35:37 +0200

    add: App element return type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div>
       <h1>Hello World!</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,4 @@ import App from "./App";
 // style
 import "./scss/main.scss";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
